5126357c81dc86dbcb4b2be245f8935d0edd6f5c
AbdulRafey476 <Abdul.Rafey476@hotmail.com>
2025-07-07 22:07:56 +0500

    add custom formatDate utility

---
+++ frontend/src/formatDate.js
@@ -0,0 +1,11 @@
+export default function formatDate(dateString) {
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "short" });
+  const year = date.getFullYear();
+  let suffix = "th";
+  if (day % 10 === 1 && day !== 11) suffix = "st";
+  else if (day % 10 === 2 && day !== 12) suffix = "nd";
+  else if (day % 10 === 3 && day !== 13) suffix = "rd";
+  return `${day}${suffix} ${month} ${year}`;
+};
